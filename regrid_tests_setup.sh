f4ed36d7a22b4db7cdc1c65522ba10aa9131cd65
David Hohn <hohn.david@gmail.com>
2022-05-25 16:03:55 +0200

    after testing on nesh

--- regrid_tests_setup.sh
+++ regrid_tests_setup.sh
@@ -1,5 +1,10 @@
 conda create -y -n test_env python=3.9
 conda activate test_env
+
 mamba install -y esmvalcore xesmf jupyterlab
-mamba install -y --no-deps esmvaltool-python
+
+wget https://raw.githubusercontent.com/ESMValGroup/ESMValTool/main/environment.yml
+mamba env update -n test_env environment.yml
+pip install https://github.com/ESMValGroup/ESMValTool
+
 jupyter-lab regrid_tests.ipynb
