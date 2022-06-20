e8edc32f9d4b44f58376217f3f31a3b67269ca08
David Hohn <dhohn@geomar.de>
2022-06-20 14:33:49 +0200

    update with new method

--- regrid_tests_setup.sh
+++ regrid_tests_setup.sh
@@ -1,10 +1,11 @@
-conda create -y -n test_env python=3.9
-conda activate test_env
+conda create -y -n test_env2 python=3.8
+conda activate test_env2
 
 mamba install -y esmvalcore xesmf jupyterlab
 
 wget https://raw.githubusercontent.com/ESMValGroup/ESMValTool/main/environment.yml
 mamba env update -n test_env environment.yml
 pip install git+https://github.com/ESMValGroup/ESMValTool
+
 
 jupyter-lab regrid_tests.ipynb
